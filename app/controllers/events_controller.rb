9118f7a57ba43a7aeb0140d9353a057c58169b5d
Raghu Betina <raghu@firstdraft.com>
2019-12-03 16:03:04 +0000

    Integrate Ransack

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,8 @@ class EventsController < ApplicationController
   end
 
   def index
-    @events = current_user.events.page(params[:page]).per(10)
+    @q = current_user.events.ransack(params[:q])
+    @events = @q.result(:distinct => true).includes(:username).page(params[:page]).per(10)
 
     render("event_templates/index.html.erb")
   end
